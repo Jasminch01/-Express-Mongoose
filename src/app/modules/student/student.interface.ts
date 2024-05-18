c447151f6438223f578174178345f5b91c2a5134
jasmin <chjasmin27@gmail.com>
2024-05-18 07:04:58 +0600

    student interface

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,38 @@
+import { Schema, model, connect } from "mongoose";
+
+export interface Guardian {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+}
+
+export interface UserName {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+}
+export interface LocalGuardian {
+    name : string;
+    contactNo : string;
+    occupation : string;
+}
+export interface Student {
+  id: string;
+  name: UserName;
+  gender: "male" | "female";
+  dateOfBirth: string;
+  email: string;
+  avatar?: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup: "A+" | "A-" | "B+" | "B-" | "O+" | "O-" | "AB+" | "AB-";
+  presentAddress: string;
+  parmanentAddress: string;
+  guardian: Guardian;
+  localGuardian : LocalGuardian;
+  profileImg? : string;
+  isActive : "active" | "inactive"
+}
